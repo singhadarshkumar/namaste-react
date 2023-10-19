90771a3d304ab37a19226d870ef777836c85a256
unknown <aksking99@gmail.com>
2023-10-20 00:39:54 +0530

    eps3

--- App.js
+++ App.js
@@ -2,16 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 //react Element 
+// const heading = <h1 id="heading">Hello World</h1>;
+// console.log(heading);
 
-const heading = React.createElement(
-    "h1", 
-    {id:"heading"}, 
-    "Hi React"
+// React Component
+// Class Based Components - old way 
+// Functional component -New way
+
+// Functional component
+const number = 10000;
+
+// component and element inside component 
+const HeadingComponent = () =>(
+    <div className="container">
+        <Title />
+        <h4>{number}</h4>
+        <h1 className="header">React Functional Component</h1>
+    </div>
 );
-console.log(heading);
 
-const jsxHeading = <h1 id="heading">Hello World</h1>;
-console.log(jsxHeading);
+const Title = () =>(
+    <div className="container">
+        <h2 className="title">Title Component</h2>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("abc"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
