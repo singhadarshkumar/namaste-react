79ea607a79565fb4fe9507dea72ba06aefe0e9ad
unknown <aksking99@gmail.com>
2023-10-27 19:52:29 +0530

    more data added in cards

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ const resList = [
     {
         data: {
             image: "https://images.pexels.com/photos/12737919/pexels-photo-12737919.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            name: "Paratha Junction",
+            name: "Aloo Paratha",
             cuisine: ["Breakfast", "Indian", "Snacks"],
             avgRating: "4.7",
             costForTwo: "40000",
@@ -89,7 +89,7 @@ const resList = [
     {
         data: {
             image: "https://images.pexels.com/photos/3631/summer-dessert-sweet-ice-cream.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            name: "Desert Store",
+            name: "Ice Cream",
             cuisine: ["Italian", "Snacks"],
             avgRating: "4.4",
             costForTwo: "50000",
@@ -99,11 +99,91 @@ const resList = [
     {
         data: {
             image: "https://images.pexels.com/photos/5560763/pexels-photo-5560763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-            name: "Dosa Samraj",
+            name: "Masala Dosa",
             cuisine: ["South-Indian", "Any Time"],
             avgRating: "4.6",
             costForTwo: "80000",
             deliveryTime: "30"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/13063303/pexels-photo-13063303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Pani Puri",
+            cuisine: ["Indian", "Fast Food"],
+            avgRating: "4.8",
+            costForTwo: "10000",
+            deliveryTime: "10"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/5409010/pexels-photo-5409010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Steam Momos",
+            cuisine: ["Chinese", "Fast Food"],
+            avgRating: "4.4",
+            costForTwo: "10000",
+            deliveryTime: "20"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/3054690/pexels-photo-3054690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Chowmin",
+            cuisine: ["Chinese", "Fast Food"],
+            avgRating: "4.0",
+            costForTwo: "12000",
+            deliveryTime: "20"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/3727196/pexels-photo-3727196.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Fried Rice",
+            cuisine: ["India", "Lunch", "Dinner"],
+            avgRating: "4.9",
+            costForTwo: "15000",
+            deliveryTime: "24"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/4475024/pexels-photo-4475024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Lassi",
+            cuisine: ["India", "Any Time", "Desert"],
+            avgRating: "4.9",
+            costForTwo: "15000",
+            deliveryTime: "24"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/1292294/pexels-photo-1292294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Lassi",
+            cuisine: ["India", "Any Time", "Desert"],
+            avgRating: "4.5",
+            costForTwo: "1000",
+            deliveryTime: "5"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Pancake",
+            cuisine: ["German", "Breakfast", "Desert"],
+            avgRating: "4.2",
+            costForTwo: "25000",
+            deliveryTime: "45"
+        }
+    },
+    {
+        data: {
+            image: "https://images.pexels.com/photos/958547/pexels-photo-958547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+            name: "Thali",
+            cuisine: ["Indian", "Lunch", "Dinner", "Non-veg Added"],
+            avgRating: "4.2",
+            costForTwo: "25000",
+            deliveryTime: "45"
         }
     }
 ];
