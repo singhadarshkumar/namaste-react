a73925acaf5ed401b2cd7520aeb9666b14cc3a45
unknown <aksking99@gmail.com>
2023-11-04 23:00:39 +0530

    useEffect usage for api data fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Body
 const Body=()=>{
@@ -9,16 +8,36 @@ const Body=()=>{
     //Local State Variable -Super powerful variable
     const [listOfRestro, setListOfRestro] = useState(resList);
 
+
+    useEffect (()=>{
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+    }
+
     return(
         <div className="main-container">
-            <div className="filter mt-3">
-                <button className="filter-btn" onClick={()=> {
-                    const filteredList = listOfRestro.filter((res)=> res.data.avgRating > 4);
-                    console.log(listOfRestro);
-                    setListOfRestro(filteredList);
-                }}>
-                    Top Rated Restro
-                </button>
+            <div className="d-flex align-items-center">
+                <div className="filter mt-3">
+                    <button className="filter-btn me-3" onClick={()=> {
+                        const filteredList = listOfRestro.filter((res)=> res.data.avgRating > 4);
+                        // console.log(listOfRestro);
+                        setListOfRestro(filteredList);
+                    }}>
+                        Top Rated Restro
+                    </button>
+                    <button className="filter-btn me-3" onClick={()=> {
+                        const filteredList = listOfRestro.filter((res)=> res.data.costForTwo > 20000);
+                        // console.log(listOfRestro);
+                        setListOfRestro(filteredList);
+                    }}>
+                        Low Price
+                    </button>
+                </div>
             </div>
             <div className="row mt-3">
                 {
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,10 +3,13 @@ const StyledCard = {
     backgroundColor: '#ffffff'
 };
 
+import { useState } from "react";
+
 // card
 const RestaurantCard = (props) => {
     const {resData} = props;
     const {image, name, cuisine, avgRating, costForTwo, deliveryTime} = resData?.data;
+    const [addItem, setAddItem] = useState("Add To Cart");
     return(
         <div className="col-lg-3 col-md-4 col-sm-6 col-12 mb-3">
             <div className="res-card" style={StyledCard}>
@@ -18,13 +21,13 @@ const RestaurantCard = (props) => {
                         <h3 className="mb-1">{name}</h3>
                         <h4>{cuisine.join(", ")}</h4>
                         <div className="d-flex align-items-center justify-content-between">
-                            <h5 className="mb-0">{avgRating} Stars</h5>
+                            <h5 className="mb-0">{"★ " + avgRating} Stars</h5>
                             <h6 className="mb-0">₹ {costForTwo / 100}</h6>
                         </div>
                         <span>{deliveryTime}mins</span>
                     </div>
-                    <button className="cart-btn">
-                        Add To Cart
+                    <button className="cart-btn" id="add" onClick={() => setAddItem(addItem == "Add To Cart" ? "Added" : "Add To Cart")}>
+                        {addItem}
                     </button>
                 </div>
             </div>
